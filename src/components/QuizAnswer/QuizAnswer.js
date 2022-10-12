7e54657408ef82886256880e9a60dcd19ce82824
yousufBD10 <yousufhassan.yh2@gmail.com>
2022-10-12 20:24:39 +0600

    all done

--- src/components/QuizAnswer/QuizAnswer.js
+++ src/components/QuizAnswer/QuizAnswer.js
@@ -29,8 +29,8 @@ const QuizAnswer = ({ questions }) => {
   };
 
   return (
-    <div className=" m-6  bg-slate-50  lg:p-16">
-      <h5 className="mb-2 text-4xl font-bold  mb-16 tracking-tight  dark:text-white">
+    <div className=" m-6 shadow-lg bg-slate-50  lg:p-16">
+      <h5 className="mb-2 lg:text-4xl font-bold  mb-16 tracking-tight  dark:text-white">
         {question}
       </h5>
       <div className="  bg-slate-50">
@@ -38,7 +38,7 @@ const QuizAnswer = ({ questions }) => {
         <div className="flex justify-center " >
           <button   
             onClick={() => handleanswer(optionquestions)}
-            className="ml-2 flex  justify-center hover:bg-sky-500 bg-blue-400 w-2/4 rounded-md  p-8 mb-4 text-3xl text-sm font-medium text-black dark:text-gray-300"
+            className="ml-2 flex  justify-center hover:bg-sky-500 bg-blue-400 lg:w-2/4 rounded-md p-4 mb-4 lg:text-3xl font-medium text-black dark:text-gray-300"
           >{optionquestions} </button>
         </div>
       ))}
@@ -46,16 +46,16 @@ const QuizAnswer = ({ questions }) => {
     </div>
      
       
-      <div className="flex justify-center  " onClick={()=> setAnswer(!answer)}> <h1 className="text-center text-2xl mr-6 font-bold ">Show correct answer</h1>
+      <div className="flex justify-center  " onClick={()=> setAnswer(!answer)}> <h1 className="text-center lg:text-2xl mr-6  font-bold ">Show correct answer</h1>
 
         {
-          answer ? <EyeSlashIcon className=" w-8  icon"/> : <EyeIcon className=" w-8 icon"/>
+          answer ? <EyeSlashIcon className="w-8  icon"/> : <EyeIcon className=" w-8 icon"/>
         }
        
       </div>
-      <h2 className={`ml-2 mt-5 text-3xl font-medium text-gray-900 ${answer ? answer : 'hidden'} dark:text-gray-300`}>
+      <div className="mt-8"><h2 className={`ml-2 mt-5 lg:text-3xl mt-16  font-medium text-gray-900 ${answer ? answer : 'hidden'} text-center p-2 rounded-md  inline  bg-green-500 dark:text-gray-300`}>
         Correct answer: {correctAnswer}
-      </h2>
+      </h2></div>
     </div>
   );
 };
